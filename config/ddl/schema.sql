5d80e2509e5a11c1a40fa9b41c6560bcc1b4a72e
Park-SeungWoo <tmd24710@naver.com>
2025-08-03 17:37:38 +0900

    feat: set docker compose file for local psql server

---
+++ config/ddl/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE urls
+(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    url TEXT,
+    description TEXT
+);
+CREATE TABLE apis
+(
+    id   INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    url_id INT,  -- FK(1toN) -> urls
+    path TEXT,
+    method TEXT,
+    description TEXT,
+    FOREIGN KEY (url_id) REFERENCES urls(id)
+);
+CREATE TABLE schemas (
+    api_id INT PRIMARY KEY ,  -- FK(1to1) -> apis
+    FOREIGN KEY (api_id) REFERENCES apis(id) ON DELETE CASCADE
+);
+CREATE TABLE fields (
+    id   INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    schema_id INT,  -- FK(1toN) -> schemas
+    name TEXT,
+    type TEXT,
+    description TEXT,
+    FOREIGN KEY (schema_id) REFERENCES schemas(api_id)
+);
