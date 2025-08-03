19d6f3ffa9f10cf3199bcaeca2b56655be529f4f
Park-SeungWoo <tmd24710@naver.com>
2025-08-03 20:04:20 +0900

    feat: add sample data insertion in start script for db

--- config/ddl/schema.sql
+++ config/ddl/schema.sql
@@ -1,28 +1,40 @@
 CREATE TABLE urls
 (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    url TEXT,
+    id          INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    url         TEXT,
     description TEXT
 );
 CREATE TABLE apis
 (
-    id   INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    url_id INT,  -- FK(1toN) -> urls
-    path TEXT,
-    method TEXT,
+    id          INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    url_id      INT, -- FK(1toN) -> urls
+    path        TEXT,
+    method      TEXT,
     description TEXT,
-    FOREIGN KEY (url_id) REFERENCES urls(id)
+    FOREIGN KEY (url_id) REFERENCES urls (id)
 );
-CREATE TABLE schemas (
-    api_id INT PRIMARY KEY,  -- FK(1to1) -> apis
-    type TEXT,
-    FOREIGN KEY (api_id) REFERENCES apis(id) ON DELETE CASCADE
+CREATE TABLE schemas
+(
+    api_id INT PRIMARY KEY, -- FK(1to1) -> apis
+    type   TEXT,
+    FOREIGN KEY (api_id) REFERENCES apis (id) ON DELETE CASCADE
 );
-CREATE TABLE fields (
-    id   INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    schema_id INT,  -- FK(1toN) -> schemas
-    name TEXT,
-    type TEXT,
+CREATE TABLE fields
+(
+    id          INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    schema_id   INT, -- FK(1toN) -> schemas
+    name        TEXT,
+    type        TEXT,
     description TEXT,
-    FOREIGN KEY (schema_id) REFERENCES schemas(api_id)
+    FOREIGN KEY (schema_id) REFERENCES schemas (api_id)
 );
+-- add test data
+INSERT INTO urls (url, description)
+VALUES ('http://localhost:8000', 'Sample api host');
+INSERT INTO apis (url_id, path, method, description)
+VALUES (1, '/users', 'GET', 'Get user data using user name and password');
+INSERT INTO schemas (api_id, type)
+VALUES (1, 'object');
+INSERT INTO fields (schema_id, name, type, description)
+VALUES (1, 'name', 'string', 'user name to identify'),
+       (1, 'password', 'string', 'user password to verify user');
