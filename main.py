98b1efe5ffb161ff587cf4524fb9b43c997742e6
Park-SeungWoo <tmd24710@naver.com>
2025-08-03 17:39:35 +0900

    feat: add command options
    - our project structure allows multiple MCP server implementation. so there needed to seelct a server to start by using some command line options

--- main.py
+++ main.py
@@ -1,5 +1,44 @@
 import asyncio
+import argparse
 
+from core.utils.ansi_styler import ANSIStyler
 from servers.local.file_server.server import FileServer
+from servers.local.db_server.server import DBServer
 
-asyncio.run(FileServer().run())
+AVAIL_SERVER_OPTIONS: list[str] = ['file', 'db']  # TODO: dynamically find existing servers
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Select a MCP server to start')
+    parser.add_argument(
+        '--server', '-S',
+        choices=AVAIL_SERVER_OPTIONS,
+        type=str,
+        nargs=1,
+        help='MCP server to start'
+    )
+    parser.add_argument(
+        '--list', '-L',
+        action='store_true',
+        help='List available MCP servers'
+    )
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    if args.list:  # ls all servers
+        print(ANSIStyler.style('Available MCP servers', fore_color='light-blue', font_style='bold'))
+        for s in AVAIL_SERVER_OPTIONS:
+            print(ANSIStyler.style(f"- {s}", fore_color='blue'))
+    elif args.server:
+        server = args.server[0]
+        match server:
+            case 'file':
+                server = FileServer()
+            case 'db':
+                server = DBServer()
+        asyncio.run(server.run())
+    else:
+        print(ANSIStyler.style("Please select an available option", fore_color='red'))
