a27ba10e4588d351e21b7f7b2a201dd063145085
Park-SeungWoo <tmd24710@naver.com>
2025-08-08 17:28:40 +0900

    feat: dynamic tool management server ex

--- servers/__init__.py
+++ servers/__init__.py
@@ -1,3 +1,4 @@
 from .stdio.db_server.server import DBServer
 from .stdio.file_server.server import FileServer
 from .sse.sample.server import SampleSSEServer
+from .dynamic.server import DynamicToolManagementServer
---
+++ servers/dynamic/server.py
@@ -0,0 +1,57 @@
+import random
+from typing import Any
+
+from mcp.types import Tool
+
+from core.bases import AbstractMcpServer
+
+
+class DynamicToolManagementServer(AbstractMcpServer):
+    def __init__(self) -> None:
+        super().__init__()
+        # TODO: DB connection
+
+    def set_handlers(self):
+        # TODO: must set prompt, resources also
+        @self.server.list_tools()
+        async def list_tools() -> list[Tool]:
+            # TODO: get from data store
+            tool1: Tool = Tool(
+                name='first_tool',
+                description='first tool',
+                inputSchema={
+                    'type': 'object',
+                    'properties': {
+                        'ping': {
+                            'type': 'string',
+                            'description': 'ping string',
+                            'default': 'MCP'
+                        }
+                    },
+                    'required': ['ping']
+                }
+            )
+            tool2: Tool = Tool(
+                name='second_tool',
+                description='second tool',
+                inputSchema={
+                    'type': 'object',
+                    'properties': {
+                        'ping': {
+                            'type': 'string',
+                            'description': 'ping string',
+                            'default': 'SSE'
+                        }
+                    },
+                    'required': ['ping']
+                }
+            )
+            return [tool1] if random.random() < 0.5 else [tool2]
+
+        @self.server.call_tool()
+        async def call_tool(tool_name: str, kwargs: dict) -> Any:
+            # TODO: handle tool by its name
+            return {
+                'pong': tool_name,
+                'recieved': kwargs
+            }
